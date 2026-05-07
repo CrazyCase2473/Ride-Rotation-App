92869ca650a95fc280409ef2f120e36369c1132b
CrazyCase2473 <deacon.mc08@gmail.com>
2026-05-07 12:26:31 -0500

    cleaned stuff up and made it look nice

---
+++ RideRotationApp2/Models/EditableShiftPosition.cs
@@ -0,0 +1,8 @@
+﻿namespace RideRotationApp2.Models;
+
+public class EditableShiftPosition
+{
+    public string PositionName { get; set; } = "";
+    public int? EmployeeId { get; set; }
+    public bool IsTraining { get; set; }
+}
